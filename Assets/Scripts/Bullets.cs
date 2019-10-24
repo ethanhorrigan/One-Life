9d02cdbb6900e5bee582c64a1768aba1c7f1f72b
EthanHorrigan <ethanhorr@gmail.com>
2019-10-24 15:54:02 +0100

    Player Shooting

--- Assets/Scripts/Bullets.cs
+++ Assets/Scripts/Bullets.cs
@@ -4,28 +4,24 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private int[] bullets;
-    [SerializeField]
-    private GameObject bullet;
+
+    public GameObject bullet;
 
     [SerializeField]
-    private Camera cam;
+    private Transform gunPos;
 
-    void Start()
-    {
-        Instantiate(bullet, new Vector2(-4f, 0), Quaternion.identity);
-    }
 
     // Update is called once per frame
     void Update()
     {
-        OnMouseDown();
+        Shoot();
     }
 
-    private void OnMouseDown()
+    public void Shoot()
     {
-        var ray = cam.ScreenPointToRay(Input.mousePosition);
-        Debug.Log(ray);
+        if (Input.GetButtonDown("Fire1")) {
+            Instantiate(bullet, gunPos.position, gunPos.rotation);
+        }
     }
+
 }
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private float shootSpeed = 10.0f;
+    private float destroyDelay = 0.5f;
+    void Update()
+    {
+        transform.Translate(Vector2.right * shootSpeed * Time.deltaTime);
+        Destroy(gameObject, destroyDelay);
+    }
+}
